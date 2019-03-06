7c1eb8c2c06b7c6bb37b4fedd676eb22bcb72206
shaiTheGuy <shay.fridboim@gmail.com>
2019-03-05 22:08:16 -0600

    Change the swiping distance to delete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvas'));
 const ctx = /** @type {CanvasRenderingContext2D} */ (canvas.getContext('2d'));
 
-const DELETE_SWIPE_DISTANCE = 200;
+const DELETE_SWIPE_DISTANCE = 150;
 
 // free draw on canvas variables
 let clickX = [];
